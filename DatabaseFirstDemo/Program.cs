b4f2b7c6fa5b64f8b81b2093dd51a41759e12011
codewithJosue <repositorydevcode@gmail.com>
2023-03-31 14:17:57 -0600

    Demo Database-First Workflow

---
+++ DatabaseFirstDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DatabaseFirstDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ctx = new DatabaseFirstDemoEntities();
+
+            var post = new Posts()
+            {
+                DatePublished = DateTime.Now,
+                Title = "title",
+                Body = "body"
+            };
+
+            ctx.Posts.Add(post);
+            ctx.SaveChanges();
+
+        }
+    }
+}
