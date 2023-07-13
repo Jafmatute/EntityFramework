127e291c6cb75d4c326bb8281cd26def1bee8de5
Jafmatute <josueari20@gmail.com>
2023-07-12 21:07:32 -0600

    Importing Stored Procedures

--- DbFirst/Program.cs
+++ DbFirst/Program.cs
@@ -1,9 +1,26 @@
-﻿namespace DbFirst
+﻿using System;
+
+namespace DbFirst
 {
-    internal class Program
+    class Program
     {
         public static void Main(string[] args)
         {
+            CallProcedure();
+        }
+
+        private static void CallProcedure()
+        {
+            var dbContext = new PlutoDbContext();
+            var courses = dbContext.GetCourses();
+
+            foreach (var course in courses)
+            {
+                Console.WriteLine($"los cursos registrados son: {course.Title}");
+            }
+
+            Console.ReadKey();
+
         }
     }
 }
