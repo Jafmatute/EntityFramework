77bcc7935eb9ef51881bf498f0bbf9c7a34677ba
Jafmatute <josueari20@gmail.com>
2023-07-14 15:20:41 -0600

    Migrations Adding a New Class

--- CodeFirstExistingDatabase/PlutoContext.cs
+++ CodeFirstExistingDatabase/PlutoContext.cs
@@ -1,7 +1,4 @@
-using System;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
-using System.Linq;
 
 namespace CodeFirstExistingDatabase
 {
@@ -15,6 +12,7 @@ namespace CodeFirstExistingDatabase
         public virtual DbSet<Author> Authors { get; set; }
         public virtual DbSet<Course> Courses { get; set; }
         public virtual DbSet<Tag> Tags { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
