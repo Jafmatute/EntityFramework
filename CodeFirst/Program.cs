bff09006fc4102db8e11c7280c67aec912e9fae9
Jafmatute <josueari20@gmail.com>
2023-07-14 14:04:42 -0600

    Code First with a New Database

---
+++ CodeFirst/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace CodeFirst
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public CourseLevel Level { get; set; }
+        public float FullPrice { get; set; }
+        public Author Author { get; set; }
+        public IList<Tag> Tags { get; set; }
+
+
+    }
+
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IList<Course> Courses { get; set; }
+
+
+    }
+
+    public class Tag
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public IList<Course> Courses { get; set; }
+
+    }
+
+    public enum CourseLevel
+    {
+        Beginner = 1,
+        Intermediated = 2,
+        Advanced = 3
+    }
+
+    public class PlutoDbContext : DbContext
+    {
+        public PlutoDbContext() : base("name=DefaultConnection")
+        {
+
+        }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
