f88ff95b219927fe4d84328d752ec50928c3e3ef
Jafmatute <josueari20@gmail.com>
2023-07-14 18:03:38 -0600

    Migrations Modifying an Existing Class

--- CodeFirstExistingDatabase/Course.cs
+++ CodeFirstExistingDatabase/Course.cs
@@ -14,9 +14,10 @@ namespace CodeFirstExistingDatabase
 
         public int Id { get; set; }
 
-        public string Title { get; set; }
+        public string Name { get; set; }
 
         public string Description { get; set; }
+
 
         public int Level { get; set; }
 
