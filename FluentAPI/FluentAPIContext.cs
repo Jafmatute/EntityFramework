cf115fd78304d5e3a2fca9b1745a491d686df373
Jafmatute <josueari20@gmail.com>
2023-07-18 21:38:58 -0600

    Demo Fluent API

---
+++ FluentAPI/FluentAPIContext.cs
@@ -0,0 +1,46 @@
+﻿using System.Data.Entity;
+
+namespace FluentAPI
+{
+    class FluentAPIContext : DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+        public FluentAPIContext() : base("name=FluentAPIContext")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>()
+                .Property(n => n.Name)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            modelBuilder.Entity<Course>()
+                .Property(d => d.Description)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            modelBuilder.Entity<Course>()
+                .HasRequired(c => c.Author)
+                .WithMany(a => a.Courses)
+                .HasForeignKey(c => c.AuthorId)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Course>()
+                .HasMany(c => c.Tags)
+                .WithMany(t => t.Courses)
+                .Map(m => m.ToTable("CourseTags"));
+
+            modelBuilder.Entity<Course>()
+                .HasRequired(c => c.Cover)
+                .WithRequiredPrincipal(c => c.Course);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
