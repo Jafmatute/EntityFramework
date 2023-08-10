f2ebe8f82104492fba5cec474314aabe2ab9d74d
Jafmatute <josueari20@gmail.com>
2023-08-10 12:20:13 -0600

    LINQ in Action

---
+++ Queries/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Linq;
+
+namespace Queries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ctx = new PlutoContext();
+
+            //Linq syntax
+            var query = from c in ctx.Courses
+                        where c.Name.Contains("c#")
+                        orderby c.Name
+                        select c;
+
+            //foreach (var course in query) Console.WriteLine(course.Name);
+
+
+            //Extension Method
+            var courses = ctx.Courses.
+                Where(c => c.Name.Contains("c#")).OrderBy(c => c.Name);
+
+            foreach (var course in courses) Console.WriteLine(course.Name);
+
+            Console.ReadKey();
+        }
+    }
+}
